bcadb2c1332d96561ed28e0db756275a40defe0f
legecha <leonardchallis@gmail.com>
2025-02-12 13:00:26 +0000

    Couple of fixes for production

--- config/stripe.php
+++ config/stripe.php
@@ -12,7 +12,7 @@ return [
     | This is the secret key used to authorise API requests to Stripe.
     |
     */
-    'secret_key' => env($isProduction ? 'STRIPE_SECRET_KEY_PROD' : 'STRIPE_SECRET_KEY_TEST'),
+    'secret_key' => env('STRIPE_SECRET_KEY_TEST'),
 
     /*
     |--------------------------------------------------------------------------
@@ -22,6 +22,6 @@ return [
     | This is the key used for public assets, such as when using Stripe.js Elemenets.
     |
     */
-    'publishable_key' => env($isProduction ? 'STRIPE_PUBLISHABLE_KEY_PROD' : 'STRIPE_PUBLISHABLE_KEY_TEST'),
+    'publishable_key' => env('STRIPE_PUBLISHABLE_KEY_TEST'),
 
 ];
